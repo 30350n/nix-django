f04a269e9306fae6fabd93e04c036a72a1bc59a8
Bobbe <maxschlecht@web.de>
2025-12-16 08:56:21 +0100

    Run tests with --no-input

--- buildDjangoApplication.nix
+++ buildDjangoApplication.nix
@@ -63,7 +63,7 @@ lib.extendMkDerivation {
         inherit doCheck;
         checkPhase =
             ''
-                python manage.py test
+                python manage.py test --no-input
                 python manage.py check --deploy --fail-level WARNING
                 find . -type d -name "__pycache__" -exec rm -rf {} +
             ''
