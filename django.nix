706247f0cb9d2dd4b6641655a4df51693e986962
Bobbe <maxschlecht@web.de>
2025-12-16 13:07:39 +0100

    Fix nixos module

--- django.nix
+++ django.nix
@@ -10,18 +10,21 @@
                     enable = mkEnableOption "enable";
                     package = mkOption {type = types.package;};
                     reverseProxy = mkOption {type = types.nullOr (types.enum ["caddy"]);};
-                    workers = 1;
+                    workers = mkOption {
+                        type = types.int;
+                        default = 1;
+                    };
                 };
             });
         };
 
     config = let
-        enabledDjangoServices = lib.filterAttrs (name: config: config.enable) config.django;
-        gunicornSocket = name: "/run/gunicorn-${name}.sock";
+        enabledDjangoServices = lib.filterAttrs (_: config: config.enable) config.services.django;
+        gunicornSocket = name: "/run/gunicorn-${name}/gunicorn.sock";
     in {
         systemd.services = let
             migrationServices = lib.mapAttrs' (name: config:
-                lib.nameValuePair "${name}_migrate" {
+                lib.nameValuePair "${name}-migrate" {
                     description = "${name} database migrations";
                     wantedBy = ["multi-user.target"];
                     after = ["network.target"];
@@ -32,46 +35,52 @@
                         WorkingDirectory = "${config.package}";
                     };
                     script = ''
-                        python manage.py migrate --no-input
+                        ${config.package.pythonVirtualEnv}/bin/python manage.py migrate --no-input
                     '';
                 })
             enabledDjangoServices;
             gunicornServices = builtins.mapAttrs (name: config: {
                 description = "${name} django application";
                 wantedBy = ["multi-user.target"];
-                after = ["${name}_migrate"];
+                requires = ["${name}-migrate.service"];
                 serviceConfig = {
                     Type = "notify";
-                    NotifyAccess = "main";
+                    NotifyAccess = "all";
                     User = name;
                     Group = name;
+                    RuntimeDirectory = "gunicorn-${name}";
                     WorkingDirectory = config.package;
                     ExecReload = "kill -s HUP $MAINPID";
                     KillMode = "mixed";
                     PrivateTmp = true;
                 };
                 script = ''
-                    python -m gunicorn ${name}.wsgi \
+                    ${config.package.pythonVirtualEnv}/bin/python -m gunicorn ${name}.wsgi \
                         --workers ${toString config.workers} \
                         --bind unix:${gunicornSocket name}
                 '';
             })
             enabledDjangoServices;
         in
-            migrationServices ++ gunicornServices;
+            migrationServices // gunicornServices;
 
-        users.users = builtins.mapAttrs (name: config: {
+        systemd.tmpfiles.rules =
+            ["d /var/www 0755 root root - -"]
+            ++ (
+                lib.mapAttrsToList (name: _: "d /var/www/${name} 0750 ${name} ${name} - -")
+                enabledDjangoServices
+            );
+
+        users.users = builtins.mapAttrs (name: _: {
             inherit name;
-            value = {
-                group = name;
-                isSystemUser = true;
-            };
+            group = name;
+            isSystemUser = true;
         })
         enabledDjangoServices;
-        users.groups = builtins.mapAttrs (name: config: {inherit name;}) enabledDjangoServices;
+        users.groups = builtins.mapAttrs (name: _: {inherit name;}) enabledDjangoServices;
 
         services.caddy.virtualHosts = lib.mapAttrs' (name: config:
-            lib.nameValuePair (lib.concatStringsSep "," config.package.settings.ALLOWED_HOSTS) {
+            lib.nameValuePair (lib.concatStringsSep ", " config.package.settings.ALLOWED_HOSTS) {
                 extraConfig = ''
                     handle_path ${config.package.settings.STATIC_URL} {
                         root * ${config.package.settings.STATIC_ROOT}
@@ -80,6 +89,6 @@
 
                     reverse_proxy unix/${gunicornSocket name}
                 '';
-            }) (lib.filterAttrs (name: config: config.reverseProxy == "caddy"));
+            }) (lib.filterAttrs (_: config: config.reverseProxy == "caddy") enabledDjangoServices);
     };
 }
